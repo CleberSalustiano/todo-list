6c64feac518896f6b00353933ca29af88cc2235d
Cleber Salustiano <kleberdejesus45@hotmail.com>
2025-05-18 14:00:34 -0300

    fix: ajusta para ser variavel de ambiente.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,8 +7,9 @@ app.use(cors());
 app.use(express.json())
 app.use(routes)
 
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3333;
 
-app.listen(3333, () => {
-  console.log('Server started on port 3333')
-})
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
 
